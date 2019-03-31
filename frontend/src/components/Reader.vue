437c906dc50dade18aaa8c44ee9fdb16f90088c7
huhn <sehe89@gmail.com>
2019-03-31 15:42:54 +0200

    add layout

--- frontend/src/components/Reader.vue
+++ frontend/src/components/Reader.vue
@@ -1,19 +1,49 @@
 <template>
-  <div 
-    class="reader"
-    v-loading="loading || waitForCard"
-    >
-    <h1>Reader</h1>
-    <h3>{{root}}</h3>
-    <h2>Ordered Products</h2>
-    <el-row :gutter="12">
-      <el-col :span="8" v-for="(product, index) in products" v-bind:key="product.id">
-        <el-card shadow="hover">
-          <h5>Product #{{index + 1}}</h5>
-          {{product.item.data.title}}, quantity: {{product.quantity}}
-        </el-card>
+  <div class="reader" v-loading="loading || waitForCard">
+    <h1>Order</h1>
+
+    <el-row>
+      <el-col :span="12">
+        <h2>Information</h2>
+        <el-form ref="form" :model="form" label-position="top">
+          <el-form-item label="ID">
+            <el-input placeholder="Please input" v-model="order.data.id" class="input-with-select">
+              <template slot="prepend">
+                <i class="el-icon-edit"></i>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="Name">
+            <el-input
+              placeholder="Please input"
+              v-model="order.data.name"
+              class="input-with-select"
+            >
+              <template slot="prepend">
+                <i class="el-icon-edit"></i>
+              </template>
+            </el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+      <el-col :span="12">
+        <h2>Packaging</h2>
+        <p>Pack all products below and check them if your're done!</p>
+        <div class="checkbox-list">
+          <el-checkbox
+            v-for="product in products"
+            :label="product.item.data.title"
+            :key="product.item.data.id"
+            border
+          >
+            #{{product.item.data.id}}: {{product.item.data.title}}
+            <el-badge :value="product.quantity" class="item" type="primary"></el-badge>
+          </el-checkbox>
+        </div>
+        <el-button type="success" v-on:click="confirmPackaging">Confirm packaging</el-button>
       </el-col>
     </el-row>
+
     <h2>Order History</h2>
     <div class="block">
       <el-timeline>
@@ -24,9 +54,8 @@
           :type="'primary'"
           :color="'#0bbd87'"
           :size="'large'"
-          :timestamp="message.timestamp | formatTimestampToDate">
-          {{message.status}} by {{message.data.name}}
-        </el-timeline-item>
+          :timestamp="message.timestamp | formatTimestampToDate"
+        >{{message.status}} by {{message.data.name}}</el-timeline-item>
       </el-timeline>
       <pre v-if="error">Error: {{error}}</pre>
     </div>
@@ -34,20 +63,22 @@
 </template>
 
 <script>
-const axios = require('axios');
-import { fetch } from '@/utils/MAM';
+const axios = require("axios");
+import { fetch } from "@/utils/MAM";
 
 export default {
-  name: 'Reader',
+  name: "Reader",
   data() {
     return {
-      root: '',
+      root: "",
       loading: false,
       waitForCard: true,
+      order: {},
       messages: [],
       products: [],
-      error: ''
-    }
+      error: "",
+      checkboxGroup: []
+    };
   },
   mounted() {
     this.readCard();
@@ -55,41 +86,59 @@ export default {
   methods: {
     readCard: function(root) {
       var self = this;
-      axios.get('http://192.168.178.135:4000/read')
-      .then(function (response) {
-        // handle success
-        self.root = response.data
-        self.loadData(self.root)
-        self.waitForCard = false
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+
+      // remove this
+      self.root =
+        "UKIXRWLNOIIP9OZGBBDGZGNLOSHMUZ9SMEEJDISWXRRWWAEPGRUCVBZFRMGDYKJAPBNKCDXBOIM9UOKJJ";
+      self.loadData(self.root);
+      self.waitForCard = false;
+      return;
+
+      axios
+        .get("http://192.168.178.135:4000/read")
+        .then(function(response) {
+          // handle success
+          self.root = response.data;
+          self.loadData(self.root);
+          self.waitForCard = false;
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     },
     loadData: async function(root) {
-          this.loading = true
-          if(root) {
-              this.root = root
-              this.message = []
-              await fetch(this.root, 'restriced', 'TEST', this.appendToMessages, this.fetchComplete);
-          } else {
-              console.log("no root defined, show search and latest views.")
-              this.loading = false
-          }
-          
-      },
-      appendToMessages(message){
-        console.log("message", message)
-        this.messages.push(message)
-      },
-      fetchComplete() {
-          this.loading = false
-          this.products = this.sortedMessages[0].data.cart
+      this.loading = true;
+      if (root) {
+        this.root = root;
+        this.message = [];
+        await fetch(
+          this.root,
+          "restriced",
+          "TEST",
+          this.appendToMessages,
+          this.fetchComplete
+        );
+      } else {
+        console.log("no root defined, show search and latest views.");
+        this.loading = false;
       }
+    },
+    appendToMessages(message) {
+      console.log("message", message);
+      this.messages.push(message);
+    },
+    fetchComplete() {
+      this.loading = false;
+      this.order = this.sortedMessages[0];
+      this.products = this.order.data.cart;
+    },
+    confirmPackaging() {
+      alert("confirmPackaging")
+    }
   },
   computed: {
     sortedMessages: function() {
@@ -100,9 +149,17 @@ export default {
       }
       return this.messages.sort(compare);
     }
-  },
-}
+  }
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.el-checkbox {
+  width: 100%;
+  margin: 5px 5px 5px 10px;
+}
+
+.checkbox-list {
+  display: inline;
+}
 </style>
