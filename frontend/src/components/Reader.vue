1436dc14c5a7b97b77629d5b9b79797b210c023e
huhn <sehe89@gmail.com>
2019-03-31 03:44:05 +0200

    add light user feedback

--- frontend/src/components/Reader.vue
+++ frontend/src/components/Reader.vue
@@ -1,8 +1,5 @@
 <template>
-  <div 
-    class="reader"
-    v-loading="loading || waitForCard"
-    >
+  <div class="reader" v-loading="loading || waitForCard">
     <h1>Reader</h1>
     <h3>{{root}}</h3>
     <h2>Ordered Products</h2>
@@ -24,9 +21,8 @@
           :type="'primary'"
           :color="'#0bbd87'"
           :size="'large'"
-          :timestamp="message.timestamp | formatTimestampToDate">
-          {{message.status}} by {{message.data.name}}
-        </el-timeline-item>
+          :timestamp="message.timestamp | formatTimestampToDate"
+        >{{message.status}} by {{message.data.name}}</el-timeline-item>
       </el-timeline>
       <pre v-if="error">Error: {{error}}</pre>
     </div>
@@ -34,62 +30,120 @@
 </template>
 
 <script>
-const axios = require('axios');
-import { fetch } from '@/utils/MAM';
+const axios = require("axios");
+import { fetch } from "@/utils/MAM";
 
 export default {
-  name: 'Reader',
+  name: "Reader",
   data() {
     return {
-      root: '',
+      root: "",
       loading: false,
       waitForCard: true,
       messages: [],
       products: [],
-      error: ''
-    }
+      error: ""
+    };
   },
   mounted() {
+    // Send a PUT request to HUE bridge
+    console.log("blue");
+    axios({
+      method: "put",
+      url:
+        "http://192.168.178.41/api/SmDBRI79hLYzldlX4aEUB8sTAScZJhULAot8hzIL/lights/1/state",
+      data: {
+        on: true,
+        bri: 254,
+        xy: [0.15, 0.15] //blue,
+      }
+    });
+
     this.readCard();
   },
   methods: {
     readCard: function(root) {
       var self = this;
-      axios.get('http://192.168.178.135:4000/read')
-      .then(function (response) {
-        // handle success
-        self.root = response.data
-        self.loadData(self.root)
-        self.waitForCard = false
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .then(function () {
-        // always executed
-      });
+      axios
+        .get("http://192.168.178.135:4000/read")
+        .then(function(response) {
+          // handle success
+          self.root = response.data;
+          self.loadData(self.root);
+          self.waitForCard = false;
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     },
     loadData: async function(root) {
-          this.loading = true
-          if(root) {
-              this.root = root
-              this.message = []
-              await fetch(this.root, 'restriced', 'TEST', this.appendToMessages, this.fetchComplete);
-          } else {
-              console.log("no root defined, show search and latest views.")
-              this.loading = false
+      this.loading = true;
+
+      if (root) {
+        this.root = root;
+        this.message = [];
+        try {
+          await fetch(
+            this.root,
+            "restriced",
+            "TEST",
+            this.appendToMessages,
+            this.fetchComplete
+          );
+        } catch (err) {
+          axios({
+            method: "put",
+            url:
+              "http://192.168.178.41/api/SmDBRI79hLYzldlX4aEUB8sTAScZJhULAot8hzIL/lights/1/state",
+            data: {
+              on: true,
+              bri: 254,
+              xy: [0.675, 0.322] //red
+            }
+          });
+        }
+
+      } else {
+        console.log("no root defined, show search and latest views.");
+        this.loading = false;
+        // Send a PUT request to HUE bridge
+        axios({
+          method: "put",
+          url:
+            "http://192.168.178.41/api/SmDBRI79hLYzldlX4aEUB8sTAScZJhULAot8hzIL/lights/1/state",
+          data: {
+            on: true,
+            bri: 254,
+            xy: [0.675, 0.322] //red
           }
-          
-      },
-      appendToMessages(message){
-        console.log("message", message)
-        this.messages.push(message)
-      },
-      fetchComplete() {
-          this.loading = false
-          this.products = this.sortedMessages[0].data.cart
+        });
       }
+    },
+    appendToMessages(message) {
+      console.log("message", message);
+      this.messages.push(message);
+    },
+    fetchComplete() {
+      // Send a PUT request to HUE bridge
+      console.log("green");
+
+      axios({
+        method: "put",
+        url:
+          "http://192.168.178.41/api/SmDBRI79hLYzldlX4aEUB8sTAScZJhULAot8hzIL/lights/1/state",
+        data: {
+          on: false,
+          bri: 254,
+          xy: [0.2, 0.7] //gree
+        }
+      });
+      this.loading = false;
+      this.products = this.sortedMessages[0].data.cart;
+    }
   },
   computed: {
     sortedMessages: function() {
@@ -100,8 +154,8 @@ export default {
       }
       return this.messages.sort(compare);
     }
-  },
-}
+  }
+};
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
